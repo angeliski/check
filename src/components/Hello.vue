2cbab0cfbbf4043fc9bfd7a7ac87dba6716415f4
Rogerio Angeliski <rogerio.angeliski@bluesoft.com.br>
2018-06-08 13:14:44 -0300

    First commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+      <form>
+        <textarea v-model="check" placeholder='Item do checklist'></textarea>
+          <button @click="adicionar()">Adicionar</button>
+      </form>
+
+      <div v-for="check in checks" :key="check">
+        <span>{{check}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      check: '',
+      checks: [],
+    };
+  },
+  methods: {
+    adicionar() {
+      this.checks.push(this.check);
+    },
+  },
+};
+</script>
+
+</style>
